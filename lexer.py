40102cacfe683fc583b9757ee6c6edd2f2fa7774
MasonCotterill <32041237+MasonCotterill@users.noreply.github.com>
2023-04-09 21:41:29 -0700

    Comments added

--- lexer.py
+++ lexer.py
@@ -1,17 +1,20 @@
+# Token class definition to represent tokens
 class Token:
+    # Token initializer with token type and lexeme
     def __init__(self, token_type, lexeme):
         self.token_type = token_type
         self.lexeme = lexeme
-
+    # Define string rep of Token object    
     def __repr__(self):
         return f"{self.token_type}\t\t{self.lexeme}"
 
-
+# Lexer function that uses file path and generates tokens
 def lexer(file_path):
     with open(file_path, "r") as file:
         source_code = file.read()
-
+# Initialize postion
     position = 0
+# Iterate until end is reached    
     while position < len(source_code):
         if source_code[position].isspace():
             position += 1
@@ -59,17 +62,18 @@ def lexer(file_path):
             yield Token("operator", source_code[position])
             position += 1
 
-        # Unexpected character
+        # Skip Unexpected character
         else:
             position += 1
 
-
+# Define main that reads input code and writes tokens to seperate file
 def main():
     input_file = "input_scode.txt"
     output_file = "output.txt"
-
+# Opens output and writes header
     with open(output_file, "w") as file:
         file.write("token\t\tlexeme\n")
+        # Iterate through tokens and write to output
         for token in lexer(input_file):
             file.write(f"{token}\n")
 
